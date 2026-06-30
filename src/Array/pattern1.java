44f03a825fb52372a82c4adefdfd3019294e8e0c
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2026-06-30 15:55:03 +0530

    star pattern

--- src/Array/pattern1.java
+++ src/Array/pattern1.java
@@ -1,5 +1,7 @@
+package Array;
+
 import java.util.Scanner;
-import java.util.Scanner;
+
 public class pattern1 {
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
