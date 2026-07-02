e792885d12414b5639f0e8bf7982c7e1a1be9eb7
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2026-07-02 11:38:04 +0530

    using for find number freq

--- src/Array/Findnumfreq.java
+++ src/Array/Findnumfreq.java
@@ -2,7 +2,19 @@ package Array;
 
 public class Findnumfreq {
     public static void main(String[] args) {
-
+        int arr[]={10,20,30,20,40,20,10};
+        frequency(arr);
+    }
+    public static void frequency(int []arr){
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]==arr[j]) {
+                    count++;
+                }
+            }
+            System.out.println("count of"+arr[i]+" is: "+count);
+        }
 
     }
 }
