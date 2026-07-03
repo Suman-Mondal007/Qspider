a9be55e782512e0110fdc2f22de7c4acfff930b8
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2026-07-03 08:36:26 +0530

    vote eligibility,upper or lower, leapyear

---
+++ src/conditions/Vote.java
@@ -0,0 +1,47 @@
+package conditions;
+import java.util.Scanner;
+public class Vote {
+    // vote checking
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter your age: ");
+//        int age= sc.nextInt();
+//        if(age>=18){
+//            System.out.println("Eligible for vote..");
+//        }else{
+//            System.out.println("Not eligible for vote!!!");
+//        }
+//    }
+
+        // uppar case or lower case
+//    public static void main(String[] args) {
+//        Scanner s=new Scanner(System.in);
+//        System.out.println("enter a character:");
+//        char ch=s.next().charAt(0);
+//        if(ch>='A'&& ch<='Z'){
+//            System.out.println("Uppercase");
+//        } else if (ch>='a' && ch<='z') {
+//            System.out.println("Lowercase");
+//        }else{
+//            System.out.println("type correctly!!");
+//        }
+//    }
+
+
+                //Leapyear or not
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter year: ");
+//        int a= sc.nextInt();
+//        if(a%4==0 && a%100!=0 || a%400==0){
+//            System.out.println("Leap-year");
+//        }else{
+//            System.out.println("Not leap-year");
+//        }
+//    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
