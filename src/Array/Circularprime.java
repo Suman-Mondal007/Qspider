f7b0a0c13bf1c82c7777edf825bc437bbb82e2b2
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2026-07-07 12:45:51 +0530

    number is circular number

---
+++ src/Array/Circularprime.java
@@ -0,0 +1,37 @@
+package Array;
+
+public class Circularprime {
+    public static boolean iscircularPrime(int num){
+        int count=count(num);
+        for(int i=1;i<=count;i++){
+            if(isPrime(num)){
+                int ld=num%10;
+                num/=10;
+                num+=ld*Math.pow(10,count-1);
+            }
+            else return false;
+        }
+        return true;
+    }
+    public static int count(int num){
+        int count =0;
+        while(num!=0){
+            int ld=num%10;
+            count++;
+            num/=10;
+        }
+        return count;
+    }
+    public static boolean isPrime(int num) {
+        int coun = 0;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int num=113;
+        System.out.println(iscircularPrime(num));
+    }
+}
