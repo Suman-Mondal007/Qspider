93f5123acd5dce25556c20df09edba8fabe23019
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2026-07-10 16:08:58 +0530

    vehicle class inheritance

---
+++ src/Inheritance/Vehile.java
@@ -0,0 +1,15 @@
+package Inheritance;
+class vehicle{
+    String brand;
+    String logo;
+}
+class car extends vehicle{
+    boolean price;
+    int mileage;
+}
+public class Vehile {
+    public static void main(String[] args) {
+        car c =new car();
+
+    }
+}
